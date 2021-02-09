4d5e948a15f0bcd8eae9072a653aaa5608d79c43
Shubham Rawat <sr856161@gmail.com>
2021-02-09 19:16:05 +0530

    fix first time news refresh

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
-const http = require('http');
-const news = require('./news.js');
-const htmlEntities = require("html-entities")
+const http = require("http");
+const news = require("./news.js");
+const htmlEntities = require("html-entities");
 
 let newsJSON = "";
 
-const refresh = () => {
-  news.getNews().then((response)=>
-  {
-    newsJSON = JSON.stringify(response);
-    // newsJSON = newsJSON.replace(/[\u{0080}-\u{FFFF}]/gu,"");
-    newsJSON = htmlEntities.decode(newsJSON);
-    console.log("NEWS REFRESHED !!!");
-  });
-}
+const refresh = async () => {
+  const response = await news.getNews();
+  newsJSON = JSON.stringify(response);
+  // newsJSON = newsJSON.replace(/[\u{0080}-\u{FFFF}]/gu,"");
+  newsJSON = htmlEntities.decode(newsJSON);
+  console.log("NEWS REFRESHED !!!");
+};
 
-refresh();
-setInterval(refresh,1000*60*60*3); //3 hours (8 times a day)
+setInterval(refresh, 1000 * 60 * 60 * 3); //3 hours (8 times a day)
 
 const hostname = "localhost";
 const port = 80;
 
-const server = http.createServer((req,res)=>{
-  res.writeHead(200, { 'Content-Type': 'application/json' } );
-  res.end(`${newsJSON}`);
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  if (newsJSON === "") {
+    //empty, for first time
+    refresh().then((r) => {
+      res.end(`${newsJSON}`);
+    });
+  } else {
+    res.end(`${newsJSON}`);
+  }
 });
 
-server.listen(port,hostname,()=>{
+server.listen(port, hostname, () => {
   console.log("server started!");
 });

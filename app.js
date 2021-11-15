0cb9fe83ef605d5e5b753fc0dd6f3927b5c21d06
Shubhamrawat5 <sr856161@gmail.com>
2021-11-15 22:04:20 +0530

    fix cors error

--- app.js
+++ app.js
@@ -14,21 +14,41 @@ const refresh = async () => {
 
 setInterval(refresh, 1000 * 60 * 60 * 3); //3 hours (8 times a day)
 
-const hostname = "localhost";
+let hostname = "localhost";
+hostname = "0.0.0.0"; //for heroku
 const port = 80;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  if (newsJSON === "") {
-    //empty, for first time
-    refresh().then((r) => {
-      res.end(`${newsJSON}`);
-    });
-  } else {
-    res.end(`${newsJSON}`);
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+    "Access-Control-Max-Age": 2592000, // 30 days
+    "Content-Type": "application/json",
+    /** add other headers as per requirement */
+  };
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(204, headers);
+    res.end();
+    return;
   }
+
+  if (["GET", "POST"].indexOf(req.method) > -1) {
+    res.writeHead(200, headers);
+    if (newsJSON === "") {
+      //empty, for first time
+      refresh().then((r) => {
+        res.end(`${newsJSON}`);
+      });
+    } else {
+      res.end(`${newsJSON}`);
+    }
+    return;
+  }
+  res.writeHead(405, headers);
+  res.end(`${req.method} is not allowed for the request.`);
 });
 
-server.listen(port, hostname, () => {
+server.listen(process.env.PORT || port, hostname, () => {
   console.log("server started!");
 });
